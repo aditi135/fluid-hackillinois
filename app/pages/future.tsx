489fde498d9d34ea9cde05a0a3272c007252af8f
Jia Gill <96542484+jiasidhugill@users.noreply.github.com>
2025-03-01 16:06:41 -0600

    Future API call works

--- app/pages/future.tsx
+++ app/pages/future.tsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import NavBar from '../../components/NavBar'; // Import the NavBar component
+
+import secrets from '../secrets';
 import styles from '../styles';
 
 export default function Future() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.logo}>fluid</Text>
-      <Text style={styles.title}>Future</Text>
+    const [userData, setUserData] = useState({"balance": 0});
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`http://api.nessieisreal.com/accounts/${secrets.account_id}?key=${secrets.NESSIE_KEY}`);
+                if (!response.ok) {
+                    throw new Error("API request failed");
+                }
+                const result = await response.json();
+                setUserData(result);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchData();
+    }, []);
 
-      <NavBar />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+        <Text style={styles.logo}>fluid</Text>
+        <Text style={styles.title}>Future</Text>
+        <Text style={styles.text}>{userData.balance}</Text>
+        <NavBar />
+        </View>
+    );
 }
