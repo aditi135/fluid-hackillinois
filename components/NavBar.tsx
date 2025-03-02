0ce4baef61ee4b34cc7d1f8526c111f617cb144c
sriya-g <sriya.gottiparthi@gmail.com>
2025-03-01 23:27:39 -0600

    navbar hover

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,28 +5,35 @@ import { useRouter } from 'expo-router'; // Import useRouter for navigation
 
 const NavBar = () => {
   const router = useRouter(); // Use the useRouter hook for navigation
-
+  const onMouseOver = (event: { target: any; }) => {
+    const btn = event.target;
+    btn.style.backgroundColor = "#A1C6EA";
+  };
+  const onMouseOut = (event: { target: any; }) => {
+    const btn = event.target;
+    btn.style.backgroundColor = '#507DBC';
+  };
   return (
     <View style={styles.navContainer}>
     
       {/* Custom Touchable button for "Go to Home" */}
-      <TouchableOpacity style={styles.button} onPress={() => router.push('pages/home')}>
-        <Text style={styles.buttonText}>Home</Text>
+      <TouchableOpacity style={styles.button} onMouseEnter={(event: { target: any; }) => onMouseOver(event)} onMouseOut={(event: any) => onMouseOut(event)} onPress={() => router.push('pages/home')}>
+        Home
       </TouchableOpacity>
 
       {/* Custom Touchable button for "Go to Past" */}
-      <TouchableOpacity style={styles.button} onPress={() => router.push('pages/past')}>
-        <Text style={styles.buttonText}>Past</Text>
+      <TouchableOpacity style={styles.button} onMouseEnter={(event: { target: any; }) => onMouseOver(event)} onMouseOut={(event: any) => onMouseOut(event)} onPress={() => router.push('pages/past')}>
+        My Past
       </TouchableOpacity>
 
       {/* Custom Touchable button for "Go to Present" */}
-      <TouchableOpacity style={styles.button} onPress={() => router.push('pages/present')}>
-        <Text style={styles.buttonText}>Present</Text>
+      <TouchableOpacity style={styles.button} onMouseEnter={(event: { target: any; }) => onMouseOver(event)} onMouseOut={(event: any) => onMouseOut(event)} onPress={() => router.push('pages/present')}>
+        My Present
       </TouchableOpacity>
 
       {/* Custom Touchable button for "Go to Future" */}
-      <TouchableOpacity style={styles.button} onPress={() => router.push('pages/future')}>
-        <Text style={styles.buttonText}>Future</Text>
+      <TouchableOpacity style={styles.button} onMouseEnter={(event: { target: any; }) => onMouseOver(event)} onMouseOut={(event: any) => onMouseOut(event)} onPress={() => router.push('pages/future')}>
+        My Future
       </TouchableOpacity>
     </View>
   );
@@ -52,6 +59,10 @@ const styles = StyleSheet.create({
     paddingVertical: 15,  // Vertical padding for the button
     justifyContent: 'center',  // Center the text vertically
     alignItems: 'center',  // Center the text horizontally
+    color: '#fff',  // White text color
+    fontSize: 18,  // Font size of the button text
+    fontWeight: 'bold',  // Make text bold
+    fontFamily: 'Lexend',
   },
   buttonText: {
     color: '#fff',  // White text color
