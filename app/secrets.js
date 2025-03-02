9f52170d4e300f13d965823481c93482812cad88
Jia Gill <96542484+jiasidhugill@users.noreply.github.com>
2025-03-02 05:12:05 -0600

    final commit before deployment

---
+++ app/secrets.js
@@ -0,0 +1,8 @@
+const secrets = {
+    NESSIE_KEY: "e014d7b72a3d221ebfac43a11b7131c9",
+    customer_id: "",
+    account_id: "", // this is for Herc's checking account
+    user_info: {},
+}
+
+export default secrets;
